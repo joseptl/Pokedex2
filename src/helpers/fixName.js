1b70e44d52bfe61e57b0a2d70df6accf590ded2a
José Torín <joseptl551@gmail.com>
2021-09-28 17:31:33 -0400

    fixName

--- src/helpers/fixName.js
+++ src/helpers/fixName.js
@@ -1,6 +1,6 @@
 const fixName = (name)=>{
     if(name.includes("-")){
-        if(name.slice(name.indexOf("-")).length>2){
+        if(name.slice(name.indexOf("-")).length>3){
             if(name.slice(name.indexOf("-")).includes("koko")||name.slice(name.indexOf("-")).includes("lele")||name.slice(name.indexOf("-")).includes("fini")||name.slice(name.indexOf("-")).includes("bulu")){
                 return name
             }else{
